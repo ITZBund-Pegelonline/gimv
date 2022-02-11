4860ba259903a6618f20e63a3020622419d9dc85
cseewald <kontakt@christian-seewald.com>
2022-02-11 11:29:51 +0100

    Adjust copyright year

--- lib/src/main/java/org/eesgmbh/gimv/client/event/ViewportMouseMoveEventHandler.java
+++ lib/src/main/java/org/eesgmbh/gimv/client/event/ViewportMouseMoveEventHandler.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 EES GmbH
+ * Copyright 2022 EES GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
